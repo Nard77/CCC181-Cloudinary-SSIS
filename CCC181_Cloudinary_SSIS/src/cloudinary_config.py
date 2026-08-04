b3c06d07a75d2aba6052236d4f599b532a17591e
Bernard Deone Barazon Dadula <161613202+Nard77@users.noreply.github.com>
2026-08-04 23:22:55 +0800

    Add files via upload

---
+++ CCC181_Cloudinary_SSIS/src/cloudinary_config.py
@@ -0,0 +1,12 @@
+import os
+import cloudinary
+from dotenv import load_dotenv
+
+load_dotenv()
+
+cloudinary.config(
+    cloud_name="sbpvdb61",
+    api_key="928985755974686",
+    api_secret="zQaVtKNrX-6ucF-VDVtteYamEak",
+    secure=True
+)
